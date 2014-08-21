9f5b32c625238d78c6bb2504c77b3cf84129c957
Ian Landsman <ian@userscape.com>
2014-08-21 16:16:25 -0400

    remove stuff

--- app/routes.php
+++ app/routes.php
@@ -104,30 +104,6 @@ Route::get('privacy', function()
 	return View::make('biz.privacy');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application 404 & 500 Error Handlers
-|--------------------------------------------------------------------------
-|
-| To centralize and simplify 404 handling, Laravel uses an awesome event
-| system to retrieve the response. Feel free to modify this function to
-| your tastes and the needs of your application.
-|
-| Similarly, we use an event to handle the display of 500 level errors
-| within the application. These errors are fired when there is an
-| uncaught exception thrown in the application.
-|
-*/
-
-Event::listen('404', function()
-{
-	return Response::error('404');
-});
-
-Event::listen('500', function()
-{
-	return Response::error('500');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -167,19 +143,4 @@ Route::filter('before', function()
 			return Redirect::to("https://{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}", 301, true);
 		}
 	}
-});
-
-Route::filter('after', function($response)
-{
-	// Do stuff after every request to your application...
-});
-
-Route::filter('csrf', function()
-{
-	if (Request::forged()) return Response::error('500');
-});
-
-Route::filter('auth', function()
-{
-	if (Auth::guest()) return Redirect::to('login');
 });
